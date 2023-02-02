69c2e03f44d318bf05eb1914244fc3d2db30cbd2
frank <frankcool321@gmail.com>
2023-02-02 22:00:39 +0800

    feat: add edit.hbs and edit feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -12,4 +12,26 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findByPk(id);
+    return res.render("edit", { todo: todo.toJSON() });
+  } catch (error) {
+    return console.log(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, isDone } = req.body;
+    await Todo.update({ name, isDone: isDone === "on" }, { where: { id } });
+    return res.redirect(`/todos/${id}`);
+  } catch (error) {
+    return console.log(error);
+  }
+});
+
+
 module.exports = router;
