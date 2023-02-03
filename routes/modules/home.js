80a26a74a3af5f0a6e6554f7f7d1fc887514fd70
frank <frankcool321@gmail.com>
2023-02-03 18:59:15 +0800

    add userId as query option in index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,8 +4,13 @@ const Todo = db.Todo;
 
 router.get("/", async (req, res) => {
   try {
+    const UserId = req.user.id;
     //! 將資料換轉換成單純的JS物件
-    const todos = await Todo.findAll({ raw: true, nest: true });
+    const todos = await Todo.findAll({
+      raw: true,
+      nest: true,
+      where: { UserId },
+    });
     return res.render("index", { todos });
   } catch (error) {
     return console.log(error);
